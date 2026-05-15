1cb60696543821057d285b5ce88db9d13138e135
sh7818 <sh7818@ic.ac.uk>
2026-05-15 13:29:32 +0100

    found some IMU setting mistakes using claude, also this is up to date code for robot with rotations, previously want uploaded

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ csv_path = "Data/"+str(time.time())+'.csv'
 arm = XArmAPI(ip)
 
 start = 0
-repetitions = 200
+repetitions = 100
 
 
 arm.clean_warn()
@@ -26,12 +26,13 @@ arm.set_mode(0)
 arm.set_state(state=0)
 arm.move_gohome(wait=False)
 
-arm.set_tool_position(x=50, y=50, z=0, roll=0, pitch=0, yaw=0, radius=None,speed=20, wait=False)
+arm.set_tool_position(x=50, y=50, z=50, roll=0, pitch=0, yaw=0, radius=None,speed=20, wait=False)
 
 
-cumul      =   [50,50,50]
-boundaries =   [[50,250],[50,250],[-50, 150]]
-
+cumul       =   [50,50,50]
+cumulr      =   [0,0,0]
+boundaries  =   [[100,200],[100,200],[-100,0]]
+boundariesr =   [[-15,15],[-15,15],[-15,15]]
 
 def random_translation(low,high,axis):
 
@@ -43,14 +44,29 @@ def random_translation(low,high,axis):
       cumul[axis] += ri
       return ri
 
+def random_rotation(low,high,axis):
+
+  ri =  random.randint(low,high)
+
+  if (ri + cumulr[axis] < boundariesr[axis][0]) or (ri + cumulr[axis] > boundariesr[axis][1]):
+      return  random_rotation(low,high,axis)
+  else:
+      cumulr[axis] += ri
+      return ri
+
+
+
 
 for i in range(1,repetitions+1):
 
     xr = random_translation(-100,100,0)
     yr = random_translation(-100,100,1)
     zr = random_translation(-50,50,2)
+    rr = random_rotation(-15,15,0)
+    pr = random_rotation(-15,15,1)
+    yr = random_rotation(-15,15,2)
 
-    arm.set_tool_position(x=int(xr), y=int(yr), z=int(zr), roll=0, pitch=0, yaw=0, radius=None,speed=40, wait=False)
+    arm.set_tool_position(x=int(xr), y=int(yr), z=int(zr), roll=int(rr), pitch=int(pr), yaw=int(yr), radius=None,speed=20, wait=False)
 
 
 
@@ -65,7 +81,7 @@ with open(csv_path, mode='w', newline='') as file:
        else: pass
 
     while(arm.get_state()[1]==1):
-           
+
            writer.writerow([time.time()]+arm.get_position(True)[1])
 
 print("finished:", csv_path)
